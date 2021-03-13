24d6b2398e37088186b4f2519be5b647881c5b27
Kangmin Han <kangminpersonal@gmail.com>
2021-03-14 01:16:45 +0900

    UI and Base Engine created

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-#라이브러리 import
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
-from bs4 import BeautifulSoup
-import requests
+#라이브러리
 
+form_class = uic.loadUiType("메인 화면.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+
+    myWindow = WindowClass()
+
+    myWindow.show()
+
+    app.exec_()
